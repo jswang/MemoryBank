working through flip logic, fixed double normalization into faiss

MemoryBank.py
@@ -6,7 +6,7 @@ import torch
 import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer
-from models import standard_config
+from models import baseline_config
 import json
 from MemoryEntry import MemoryEntry
 
@@ -15,7 +15,7 @@ logging.set_verbosity_error()
 
 
 class MemoryBank:
-    def __init__(self, config=standard_config):
+    def __init__(self, config=baseline_config):
         """
         Create a MemoryBank model based on configuration.
         """
@@ -51,7 +51,7 @@ class MemoryBank:
         self.feedback = config["feedback_type"]
         if self.feedback == "topic":
             self.entities_dict = {k: [] for k in json.load(
-                open("silver_facts.json")).keys()}
+                open("data/silver_facts.json")).keys()}
         else:
             self.entities_dict = dict()
         self.n_feedback = 3
@@ -86,7 +86,7 @@ class MemoryBank:
         if self.feedback == "relevant":
             s_embed = self.encode_sent(questions)
             retrieved, I = self.retrieve_from_index(
-                s_embed)  # TODO here
+                s_embed)
             contxt = " ".join(
                 [retrieved[i].get_declarative_statement() for i in I[:self.n_feedback]])
         else:
@@ -119,7 +119,6 @@ class MemoryBank:
         return [a.split('$answer$ = ')[1] for a in ans]
 
     def add_to_index(self, s_embed: np.array):
-        s_embed /= torch.unsqueeze(torch.norm(s_embed, dim=1), 1)
         self.index.add(s_embed.cpu().detach().numpy())
 
     def retrieve_from_index(self, s_new) -> Tuple[List[MemoryEntry], np.array]:
@@ -128,11 +127,10 @@ class MemoryBank:
         s_new is a stacked Tensor, first dimension is batch
         """
         # faiss.normalize_L2(x=s_new)
-        s_new /= np.linalg.norm(s_new)
+        s_new /= torch.unsqueeze(torch.norm(s_new, dim=1), 1)
         # I is the indices
-        _, _, I = self.index.range_search(x=s_new, thresh=self.threshold)
-
-        # TODO: Do we want any additional criteria?
+        _, _, I = self.index.range_search(
+            x=s_new.cpu().detach().numpy(), thresh=self.threshold)
         retrieved = []
         for i in range(I.shape[-1]):
             e = self.mem_bank[I[i]]
@@ -140,6 +138,9 @@ class MemoryBank:
         return retrieved, I
 
     def flip_or_keep(self, retrieved: List[MemoryEntry], inds, entry: MemoryEntry) -> MemoryEntry:
+        """
+        Given retrieved MemoryEntry
+        """
         hypothesis_score = entry.get_confidence()
         premise_scores = [r.get_confidence() for r in retrieved]  # for marty
 
@@ -222,7 +223,6 @@ class MemoryBank:
         if self.enable_flip:
             s_embed = self.encode_sent(
                 [s.get_declarative_statement() for s in statements])
-            # TODO probably something wrong here
             retrieved, inds = self.retrieve_from_index(s_embed)
             statements = [self.flip_or_keep(
                 retrieved, inds, s) for s in statements]

models.py
@@ -2,8 +2,8 @@ import torch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"Using device {device}")
 
-# Our first configuration for the model
-standard_config = {
+# Model tries to flip sentences, no feedback
+flip_config = {
     # NLI model which outputs relation of premise and hypothesis
     "nli_model": "ynie/roberta-large-snli_mnli_fever_anli_R1_R2_R3-nli",
     # Question answering model
@@ -17,7 +17,7 @@ standard_config = {
     # Threshold used to lookup in faiss indexer
     "sentence_similarity_threshold": 0.6,
     # Whether we flip answers to questions
-    "enable_flip": False,
+    "enable_flip": True,
     # Whether we add feedback: ("revelant", "topic", None)
     "feedback_type": None,
     # Device: defaults to whatever is available

run.py
@@ -20,40 +20,11 @@ test_sentences = [('Is an american bison a mammal?', 'yes'),
                   ('Is an american bison an amniote?', 'yes')]
 
 
-def evaluate_baseline(mem_bank, data, output_file, batch_size=400):
-    """
-    Evaluate question answering model's raw ability to answer questions
-    """
-    f1_scores = []
-    accuracies = []
-    consistencies = []
-
-    with open(output_file, "w") as f:
-        questions = [q for (q, _) in data]
-        a_truth = torch.tensor([1 if a == "yes" else 0 for (_, a) in data])
-        for i in tqdm(range(0, len(questions), batch_size)):
-            end = i+min(batch_size, len(questions))
-            q_batch = questions[i:end]
-            a_pred_batch = mem_bank.ask_questions(q_batch)
-            for j in range(len(q_batch)):
-                q, a_p = q_batch[j], a_pred_batch[j]
-                f.write(f"{q}, {a_p}\n")
-            a_pred_batch = torch.tensor(
-                [1 if a == "yes" else 0 for a in a_pred_batch])
-
-            f1_scores += [sklearn.metrics.f1_score(
-                a_truth[i:end], a_pred_batch)]
-            accuracies += [torch.sum(a_truth[i:end] ==
-                                     a_pred_batch) / batch_size]
-
-            consistencies += [1]
-
-            print(
-                f"Batch {i} accuracy: {accuracies[-1]}, f1_score: {f1_scores[-1]}, consistency: {consistencies[-1]}")
-    return f1_scores, accuracies, consistencies
-
-
 def evaluate_model(mem_bank, data, constraints=None, batch_size=400):
+    """
+    Given a model and data containing questions with ground truth, run through
+    data in batches. If constraints is None, check consistency as well.
+    """
     a_truth = torch.tensor([1 if a == "yes" else 0 for (_, _, a) in data])
     f1_scores = []
     accuracies = []
@@ -73,15 +44,14 @@ def evaluate_model(mem_bank, data, constraints=None, batch_size=400):
 
 
 if __name__ == "__main__":
-    mem_bank = MemoryBank(baseline_config)
-    data = utils.json_to_tuples(json.load(open("silver_facts.json")))
-    constraints = json.load(open("constraints_v2.json"))
+    data = utils.json_to_tuples(json.load(open("data/silver_facts.json")))
+    constraints = json.load(open("data/constraints_v2.json"))
     constraints = [Implication(c) for c in constraints["links"]]
+
+    # Evaluate baseline (no flipping) on silver facts
+    mem_bank = MemoryBank(baseline_config)
     f1_scores, accuracies, consistencies = evaluate_model(
         mem_bank, data, constraints)
-
-    # f1_scores, accuracies, consistencies = evaluate_baseline(
-    #     mem_bank, test_sentences, "baseline_output.txt")
     b = [i for i in range(len(f1_scores))]
     plt.plot(b, f1_scores, label="F1 scores")
     plt.plot(b, accuracies, label="Accuracy")
@@ -89,10 +59,17 @@ if __name__ == "__main__":
     plt.legend()
     plt.xlabel("After Batch")
     plt.title("Raw model benchmarks")
-    plt.savefig("raw_benchmarks.png")
-    plt.show()
+    plt.savefig("figures/raw_benchmarks.png")
 
-    # Evaluate baseline on silver facts
-    # data = utils.json_to_qas(json.load(open("silver_facts.json")))
-    # utils.write_to_text(data, "silver_facts.txt")
-    # f1_scores, accuracies, consistencies = evaluate_baseline(mem_bank, data, "baseline_output.txt")
+    # Evaluate flipping
+    mem_bank = MemoryBank(flip_config)
+    f1_scores, accuracies, consistencies = evaluate_model(
+        mem_bank, data, constraints)
+    b = [i for i in range(len(f1_scores))]
+    plt.plot(b, f1_scores, label="F1 scores")
+    plt.plot(b, accuracies, label="Accuracy")
+    plt.plot(b, consistencies, label="Consistency")
+    plt.legend()
+    plt.xlabel("After Batch")
+    plt.title("Flipping model benchmarks")
+    plt.savefig("figures/flip_benchmarks.png")